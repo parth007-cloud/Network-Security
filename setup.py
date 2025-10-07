f461f9266e652d902cbabf76f3122b312b3b848f
Parth Gujarati <parthgujarati07@gmail.com>
2025-10-07 17:22:50 +0530

    setup.py file ready

---
+++ setup.py
@@ -0,0 +1,36 @@
+'''
+The setup.py file is an essential part of packaging and distributing python projects...
+'''
+
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements()->List[str]:
+    """
+    Thisss function will return list of requirements 
+
+    """
+    requirement_lst:List[str]=[]
+    try:
+        with open('requirements.txt','r') as file:
+            lines = file.readlines()
+
+            for line in lines:
+                requirement = line.strip()
+
+                if requirement and requirement!= '-e .':
+                    requirement_lst.append(requirement)
+    except FileNotFoundError:
+        print("requirements.txt file not found")
+    
+    return requirement_lst
+
+setup(
+    name = "Network security",
+    version = "0.0.1",
+    author = "Parth Gujarati",
+    author_email= "parthgujarati07@gmail.com",
+    packages= find_packages(),
+    install_requires = get_requirements()
+)
+print(get_requirements())    
